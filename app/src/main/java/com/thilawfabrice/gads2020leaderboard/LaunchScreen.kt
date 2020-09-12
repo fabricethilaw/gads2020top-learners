c5c6a4362ab9da95a717e966934e4fbf72c1d852
Fabrice Thilaw <fabricethilaw@users.noreply.github.com>
2020-09-12 10:30:15 +0100

    ft: display GADS logo on app launch

---
+++ app/src/main/java/com/thilawfabrice/gads2020leaderboard/LaunchScreen.kt
@@ -0,0 +1,31 @@
+package com.thilawfabrice.gads2020leaderboard
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class LaunchScreen : AppCompatActivity() {
+
+    private val SPLASH_DISPLAY_LENGHT = 1200
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+        displayHomeScreen(SPLASH_DISPLAY_LENGHT.toLong())
+    }
+
+    /**
+     * Display Home screen automatically
+     * after the specified amount of time elapses
+     */
+    private fun displayHomeScreen(delayMillisecondsTime: Long) {
+        Handler(Looper.getMainLooper()).postDelayed({
+            //  val intent = Intent(this, HomeScreen::class.java)
+            //  startActivity(intent)
+            finish()
+
+        }, delayMillisecondsTime)
+    }
+
+}
